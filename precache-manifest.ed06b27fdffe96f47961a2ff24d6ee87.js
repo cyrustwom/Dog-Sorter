a51cbc31f2684e383de03eca647d91ed921f8c5d
cyrus <cyrus_twombly@brown.edu>
2018-11-28 15:35:14 -0500

    Updates

---
+++ precache-manifest.ed06b27fdffe96f47961a2ff24d6ee87.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b1dd7358efd6874126497cd92ec4bbe",
+    "url": "/Dog-Sorter/static/media/Bernard.8b1dd735.jpg"
+  },
+  {
+    "revision": "cf7332d0f1a48403e121",
+    "url": "/Dog-Sorter/static/css/main.6232d452.chunk.css"
+  },
+  {
+    "revision": "d154b4e04d15091cd6b5",
+    "url": "/Dog-Sorter/static/js/1.d154b4e0.chunk.js"
+  },
+  {
+    "revision": "3cd13fdb3f36776078d1",
+    "url": "/Dog-Sorter/static/js/runtime~main.3cd13fdb.js"
+  },
+  {
+    "revision": "467082b3528858beb966811fec4169b4",
+    "url": "/Dog-Sorter/static/media/puplogorightcolor.467082b3.png"
+  },
+  {
+    "revision": "148134977cc76165c39dc4fa5d4de052",
+    "url": "/Dog-Sorter/static/media/bluedog.14813497.jpg"
+  },
+  {
+    "revision": "6ead36604e5dd8bdd7544b40ffa0d670",
+    "url": "/Dog-Sorter/static/media/Golden.6ead3660.jpg"
+  },
+  {
+    "revision": "8f1671d47c66539bc26167e83e0bcdee",
+    "url": "/Dog-Sorter/static/media/Cocker.8f1671d4.jpg"
+  },
+  {
+    "revision": "f5474a652409522e31deb00f9c70765a",
+    "url": "/Dog-Sorter/static/media/Setter.f5474a65.jpg"
+  },
+  {
+    "revision": "52e990cf12890b388d79e436db50ec19",
+    "url": "/Dog-Sorter/static/media/Foxhound.52e990cf.jpg"
+  },
+  {
+    "revision": "e79ff4b83cc1ace25fbc9e620fa29631",
+    "url": "/Dog-Sorter/static/media/Basset.e79ff4b8.jpg"
+  },
+  {
+    "revision": "34d14258a7a010a1ce8dd214318645ed",
+    "url": "/Dog-Sorter/static/media/Dachshund.34d14258.jpg"
+  },
+  {
+    "revision": "28c94f9f22859f7763e6847d46d1d2c5",
+    "url": "/Dog-Sorter/static/media/Malamute.28c94f9f.jpg"
+  },
+  {
+    "revision": "7f83fc08d20faf3b62bc7ac6e7ab2102",
+    "url": "/Dog-Sorter/static/media/Newfoundland.7f83fc08.jpg"
+  },
+  {
+    "revision": "cf7332d0f1a48403e121",
+    "url": "/Dog-Sorter/static/js/main.cf7332d0.chunk.js"
+  },
+  {
+    "revision": "d391101a279de8066b17340f3fc438f3",
+    "url": "/Dog-Sorter/static/media/Irish-Terrier.d391101a.jpg"
+  },
+  {
+    "revision": "3702d5f6bbae46cdb79f4d66f2f344ea",
+    "url": "/Dog-Sorter/static/media/Norfolk-Terrier.3702d5f6.jpg"
+  },
+  {
+    "revision": "19b4f73ba5936f5e63fa722649c8ae7d",
+    "url": "/Dog-Sorter/static/media/Cairn.19b4f73b.jpg"
+  },
+  {
+    "revision": "08c38d99a2003e9ee11c49e688d71b87",
+    "url": "/Dog-Sorter/static/media/Havanese.08c38d99.jpg"
+  },
+  {
+    "revision": "b24d3f737405bacd76ab4b5ef8a33c9e",
+    "url": "/Dog-Sorter/static/media/Pug.b24d3f73.jpg"
+  },
+  {
+    "revision": "191f52d09e3437fd24e8a490f7163ae1",
+    "url": "/Dog-Sorter/static/media/Pom.191f52d0.jpg"
+  },
+  {
+    "revision": "e229a4e9d5fa588b5dc5909e94d1aa77",
+    "url": "/Dog-Sorter/static/media/Boston-Terrier.e229a4e9.jpg"
+  },
+  {
+    "revision": "4546c62920c7160ad2a8ba2a3b5d7536",
+    "url": "/Dog-Sorter/static/media/Bulldog.4546c629.jpg"
+  },
+  {
+    "revision": "8678caf848de858d9ff22da8a0fca98e",
+    "url": "/Dog-Sorter/static/media/Shiba.8678caf8.jpg"
+  },
+  {
+    "revision": "c9e4e1bc851d15c31c790089b890e146",
+    "url": "/Dog-Sorter/static/media/Border-Collie.c9e4e1bc.jpg"
+  },
+  {
+    "revision": "22af044ff211542d3d3665544bc5529d",
+    "url": "/Dog-Sorter/static/media/Shetland.22af044f.jpg"
+  },
+  {
+    "revision": "5b4c2ab1a336eaa2aa98406ec2118e57",
+    "url": "/Dog-Sorter/static/media/Briard.5b4c2ab1.jpg"
+  },
+  {
+    "revision": "a6cc5c7671577f4b9820cb6d7e1a6e78",
+    "url": "/Dog-Sorter/index.html"
+  }
+];
